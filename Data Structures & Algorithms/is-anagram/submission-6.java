7f9501b3150552ee7942d9aeff2fe3585085fc5a
ramyamuthineni23 <62419607+ramyamuthineni23@users.noreply.github.com>
2026-06-06 17:55:55 -0500

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        if (n != m)
+            return false;
+
+        int[] sArray = new int[26];
+        int[] tArray = new int[26];
+
+        for (int i = 0; i < n; i++) {
+            sArray[s.charAt(i) - 'a']++;
+            tArray[t.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (sArray[i] != tArray[i])
+                return false;
+        }
+        return true;
+    }
+}
